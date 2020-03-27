a89af265afb0fa0538de65d3faec0efe6e389927
andylamax <andylamax@programmer.net>
2020-03-27 05:58:01 +0300

    Releasing 1.0.0-alpha1

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("maven-publish")
+}
+
+android { configureAndroid() }
+
+kotlin {
+    android {
+        compilations.all {
+            kotlinOptions { jvmTarget = "1.8" }
+        }
+        publishLibraryVariants("release")
+    }
+
+    jvm {
+        compilations.all {
+            kotlinOptions { jvmTarget = "1.8" }
+        }
+    }
+
+    js {
+        compilations.all {
+            kotlinOptions {
+                metaInfo = true
+                sourceMap = true
+                moduleKind = "commonjs"
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(asoftTest("metadata"))
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+
+        val androidTest by getting {
+            dependencies {
+                implementation(asoftTest("android"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation(asoftTest("jvm"))
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(asoftTest("js"))
+            }
+        }
+    }
+}
